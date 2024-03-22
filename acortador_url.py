fdb81da7aa83aafe1ffb0aba85d64773de65d7e2
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-03-22 19:55:47 -0300

    Intento1

---
+++ acortador_url.py
@@ -0,0 +1,9 @@
+import pyshorteners
+import streamlit as st
+
+def shorten_url(url):
+    s = pyshorteners.Shortener()
+    shortened_url = s.tinyurl.short(url)
+    return shortened url
+
+# creamos app web con
